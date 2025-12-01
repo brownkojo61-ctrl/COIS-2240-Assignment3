514fa66d7232a1fe24c21275b71a897ac3def5d8
Emmanuel. Student <brownkojo61@gmail.com>
2025-11-30 21:31:23 -0500

    Add testRentAndReturnVehicle and update RentalSystem methods to return boolean

--- RentalSystem.java
+++ RentalSystem.java
@@ -150,27 +150,31 @@ public class RentalSystem {
     }
 
 
-    public void rentVehicle(Vehicle vehicle, Customer customer, LocalDate date, double amount) {
+    public boolean rentVehicle(Vehicle vehicle, Customer customer, LocalDate date, double amount) {
         if (vehicle.getStatus() == Vehicle.VehicleStatus.Available) {
             vehicle.setStatus(Vehicle.VehicleStatus.Rented);
             RentalRecord record = new RentalRecord(vehicle, customer, date, amount, "RENT");
             rentalHistory.addRecord(record);
             saveRecord(record); 
             System.out.println("Vehicle rented to " + customer.getCustomerName());
+            return true;
         } else {
             System.out.println("Vehicle is not available for renting.");
+            return false; 
         }
     }
 
-    public void returnVehicle(Vehicle vehicle, Customer customer, LocalDate date, double extraFees) {
+    public boolean returnVehicle(Vehicle vehicle, Customer customer, LocalDate date, double extraFees) {
         if (vehicle.getStatus() == Vehicle.VehicleStatus.Rented) {
             vehicle.setStatus(Vehicle.VehicleStatus.Available);
             RentalRecord record = new RentalRecord(vehicle, customer, date, extraFees, "RETURN");
             rentalHistory.addRecord(record);
             saveRecord(record);  
             System.out.println("Vehicle returned by " + customer.getCustomerName());
+            return true;
         } else {
             System.out.println("Vehicle is not rented.");
+            return false; 
         }
     }
    
